44636eb647673bf64aff45f9a795922ea60cd03f
q <69131802+earthmite@users.noreply.github.com>
2022-03-04 14:03:22 -0800

    revert if broken

--- script.js
+++ script.js
@@ -108,11 +108,8 @@ function createMessageElement(text, member) {
   el.appendChild(createMemberElement(member));
   el.appendChild(document.createTextNode(text));
   el.className = 'message';
-  window.setInterval(function() {
-    var elem = document.getElementById('data');
-    elem.scrollTop = elem.scrollHeight;
-  }, 5000);
-  
+  var objDiv = document.getElementById('div');
+objDiv.scrollTop = objDiv.scrollHeight;
   return el;
 }
 
