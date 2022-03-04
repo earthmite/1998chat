083f5e51070f65928fd5ff88982151e9b76f716c
q <69131802+earthmite@users.noreply.github.com>
2022-03-04 14:21:43 -0800

    ?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const CLIENT_ID = 'JbLYzSLLVR0Pyc4n';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { 
-    name: prompt("What's your name?"),
+    name: prompt("Username?"),
     color: getRandomColor(),
   },
 });
