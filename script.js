46e23661e7bfecfeaf5904dff6def21e339c7133
q <69131802+earthmite@users.noreply.github.com>
2022-03-10 13:32:33 -0800

    please?

--- script.js
+++ script.js
@@ -111,9 +111,9 @@ function createMessageElement(text, member) {
   el.appendChild(createMemberElement(member));
   el.appendChild(document.createTextNode(text));
   el.className = 'message';
-  if (message == 'nigger',
-  'gay porn') return
-  else;
+  if (message.includes('gay porn')) { 
+    return
+  }
   el.scrollIntoView(true);
   var objDiv = document.getElementById("message");
 objDiv.scrollTop = objDiv.scrollHeight;
